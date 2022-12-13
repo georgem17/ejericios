647ea60774dd2c5fe96961c2c7d76554b5cdb0c7
georgem17 <jorgeagostoa@gmail.com>
2022-12-13 11:27:31 +0100

    ejecicio_completo

---
+++ app.js
@@ -0,0 +1,64 @@
+// ejercicio 1 ♥
+var nombre = "Jorge "
+var apellido = "Marmanillo "
+var edad = 18;
+var ciudad = "Lima"
+
+console.log("Mi nombre es " + nombre + apellido + edad + " "
+            + ciudad)
+
+// ejercicio 2 ♥
+var suma = [12,24,13]
+var resultado = suma[0] + suma[1] + suma[2]
+console.log(resultado)
+
+// ejercicio 3 ♥
+var user = {
+            nombre: "Jorge ",
+            apellido: "Marmanillo ",
+            edad: 20,
+}
+var completo = user.nombre + user.apellido;
+console.log(completo)
+
+// ejercicio 3 ♥
+
+var alumnos = [
+  {
+    nombre:"Juan", 
+    email:"juan@gmail.com", 
+    edad:23,
+  },
+  {
+    nombre:"Olga", 
+    email:"olga@gmail.com", 
+    edad:10,
+  },
+  {
+    nombre:"Josselyn", 
+    email:"josselyn@gmail.com", 
+    edad:42,
+  },
+  {
+    nombre:"Italo", 
+    email:"italo@gmail.com", 
+    edad:30,
+  },
+  {
+    nombre:"Hugo", 
+    email:"hugo@gmail.com", 
+    edad:25,
+  },
+]
+
+const correos =[
+  alumnos[0].email,
+  alumnos[1].email,
+  alumnos[2].email,
+  alumnos[3].email,
+  alumnos[4].email
+]
+
+console.log(correos)
+
+// 👍👍👍👍
